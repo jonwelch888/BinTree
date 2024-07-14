726c1d6c9b4ab850e2a04f2b7b98f735d0c6e10a
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-14 06:22:29 -0500

    created bintree.h
    #1

--- bintree.h
+++ bintree.h
@@ -1,12 +1,65 @@
-/*
- * Your comment header here
- */
+/**************************************
+* Name: Jon Welch
+* Assignment: 8
+* Purpose of the file: This file contains the definition of the BinTree class.
+* It declares the structure for Data, DataNode, and the BinTree class methods and attributes.
+***************************************/
 
-#ifndef BINTREE_BINTREE_H
-#define BINTREE_BINTREE_H
+#ifndef BINTREE_H
+#define BINTREE_H
 
+#include <iostream>
+#include <string>
 
+struct Data
+{
+    int id;
+    std::string data;
+};
 
+struct DataNode
+{
+    Data data;
+    DataNode* left;
+    DataNode* right;
+};
 
-#endif /* BINTREE_BINTREE_H */
+class BinTree
+{
+    private:
+    DataNode* root;
+    int count;
+
+    void clear(DataNode*);
+    bool addNode(DataNode*, DataNode**);
+    DataNode* removeNode(int, DataNode*);
+    DataNode* findMin(DataNode*);
+    bool getNode(Data*, int, DataNode*);
+    bool contains(int, DataNode*);
+    int getHeight(DataNode*);
+    void displayPreOrder(DataNode*);
+    void displayPostOrder(DataNode*);
+    void displayInOrder(DataNode*);
+
+    public:
+    BinTree();
+    ~BinTree();
+
+    bool isEmpty();
+    int getCount();
+    bool getRootData(Data*);
+    void displayTree();
+    void clear();
+    bool addNode(int, const std::string&);
+    bool removeNode(int);
+    bool getNode(Data*, int);
+    bool contains(int);
+    int getHeight();
+    void displayPreOrder();
+    void displayPostOrder();
+    void displayInOrder();
+};
+
+#endif
+
 

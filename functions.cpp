956397e4c2c0cf88cad8c18fdf7752a850ec408e
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-14 08:12:14 -0500

    Trying to troubleshoot
    Terminal is throwing error 
    
    [functions.cpp:11:13: error: no member named 'rand' in namespace 'std'; did you mean simply 'rand'?
        return (std::rand() % (max - min + 1)) +min;
                ^~~~~~~~~
                rand
    /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/stdlib.h:162:6: note: 'rand' declared here
    int      rand(void) __swift_unavailable("Use arc4random instead.");
             ^
    1 error generated.]

--- functions.cpp
+++ functions.cpp
@@ -8,7 +8,7 @@
 // do not modify this function
 int random_range(int min, int max) {
     //returns a random number between min and max inclusive
-    return (std::rand() % (max - min + 1)) +min;
+    return (rand() % (max - min + 1)) +min;
 }
 
 // you may add more functions here if you need them
